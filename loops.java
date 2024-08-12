125ca4bd5da25a38a8a1799a1a9a95b2e27f0ff2
Gabriel Venturini <dev.venturini@gmail.com>
2024-08-11 22:33:45 -0300

    loops

---
+++ loops.java
@@ -0,0 +1,17 @@
+public class loops {
+    public static void main(String[] args) {
+        // for loop
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        } // print 0 - 10
+
+        // while loop
+        int j = 0;
+
+        while (j != 10) {
+            System.out.println(j);
+            j++;
+        } // print 0 - 9
+    }
+}
